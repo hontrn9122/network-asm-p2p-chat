a7dbaf04799f101326bb357d7d211cf3a77ac9a4
HoangTran <jonny.tran12902@gmail.com>
2022-12-08 21:21:36 +0700

    refactor folder name
    Signed-off-by: HoangTran <jonny.tran12902@gmail.com>

--- UI Test file/theme.py
+++
@@ -1,9 +0,0 @@
-#define font and colors
-my_font = ('haveltica', 14)
-my_font_small = ('heveltica', 10)
-my_font_italic_underscore = ('helvetica', 14, 'italic', 'underline')
-darkgreen = "#2A4731"
-lightgreen = "#6F8C64"
-yellow = "#FDD584"
-black = "#0D110F"
-white = "#FFFFFF"
