44eb24fc73b6cf4616fa4e3cf94ac190829095d2
HuyHoang0712 <hoangdinhhuy0712@gmail.com>
2022-12-09 19:51:23 +0700

    server

--- src/server.py
+++ src/server.py
@@ -1,4 +1,4 @@
-import tkinter, socket, threading, os
+import tkinter, socket, threading, os, json
 from tkinter import *
 from tkinter import messagebox
 from theme import *
@@ -7,7 +7,7 @@ from tkinter import filedialog
 
 # Defining constant
 HOSTNAME = 'localhost'
-PORT = socket.gethostbyname(HOSTNAME)
+PORT = 50000
 ENCODER = 'utf-8'
 BYTESIZE = 1024
 
@@ -23,7 +23,13 @@ client_name_list = []
 #      friend_ip[],
 #      friend_port[]
 #  }
-user_account_list = []
+user_account_list = {
+    'huyhoang' : {
+        'email' : 'huy@gamil.com',
+        'password': '123456',
+        'friend_list': [],
+    }
+}
 
 # Create a server socket using TCP protocol
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -39,21 +45,36 @@ def connect_client():
 
 
 def verify_account(client_socket, client_address):
-    while True:
-        flag, message = client_socket.recv(BYTESIZE).decode(ENCODER)
-        if flag == 'LOGIN':
-            userId, password = message.split(':')
-            
-        elif flag == "REGISTER":
-            pass
-        elif flag == "FORGOTPASS":
-            pass
+    flag, message = client_socket.recv(BYTESIZE).decode(ENCODER)
+    if flag == 'LOGIN':
+        userId, password = message.split(':')
+        if userId not in user_account_list:
+            client_socket.send('FAIL'.encode(ENCODER))
+            return
+        elif password == user_account_list[userId]['password']:
+            client_socket.send(user_account_list[userId]['email'].encode(ENCODER))
+            friend_name = json.dumps(user_account_list[userId]['friend_list'])
+            if friend_name == '[]':
+                client_socket.send('NULL'.encode(ENCODER))
+            else:
+                client_socket.send(friend_name.encode(ENCODER))
+            client_socket.send('NULL'.encode(ENCODER))
+            client_socket.send('NULL'.encode(ENCODER))
 
-def recieve_message(client_socket):
-    try:
-        flag, message = client_socket.recv(BYTESIZE).decode(ENCODER)
-        if flag == 'LOGIN':
-            pass
-        if 
-    except:
+
+
+    elif flag == "REGISTER":
         pass
+    elif flag == "FORGOTPASS":
+        pass
+
+# def recieve_message(client_socket):
+#     try:
+#         flag, message = client_socket.recv(BYTESIZE).decode(ENCODER)
+#         if flag == 'LOGIN':
+#             pass
+#         if
+#     except:
+#         pass
+
+connect_client()
