6e80ce7c61c2d514aff2055093fdd05a75920cd9
HuyHoang0712 <hoangdinhhuy0712@gmail.com>
2022-12-09 09:12:28 +0700

    vscode update

---
+++ src/server.py
@@ -0,0 +1,37 @@
+import tkinter, socket, threading, os
+from tkinter import *
+from tkinter import messagebox
+from theme import *
+from tkinter.messagebox import askyesno, showerror
+from tkinter import filedialog
+
+# Defining constant
+HOSTNAME = 'localhost'
+PORT = socket.gethostbyname(HOSTNAME)
+ENCODER = 'utf-8'
+BYTESIZE = 1024
+
+# Manage online client
+client_socket_list = []
+client_name_list = []
+
+# Manage user account and their friends
+# Structure of User Account:
+#  userId: {
+#      password<string>,
+#      email<string>,
+#      friend_ip[],
+#      friend_port[]
+#  }
+user_account_list = []
+
+# Create a server socket using TCP protocol
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_socket.bind((HOSTNAME, PORT))
+server_socket.listen()
+
+# Connecting client
+def connect_client():
+    while True:
+        client_socket, client_address = server_socket.accept()
+
